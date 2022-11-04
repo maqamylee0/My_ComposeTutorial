4cec077a034f6fb89225ed68b249571bee4db36c
maqamylee0 <maqamylee0@gmail.com>
2022-11-04 18:39:16 +0300

    Add design

--- app/src/main/java/com/tech4dev/mycomposetutorial/MainActivity.kt
+++ app/src/main/java/com/tech4dev/mycomposetutorial/MainActivity.kt
@@ -3,10 +3,18 @@ package com.tech4dev.mycomposetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+
+import java.nio.file.Files.size
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,10 +30,21 @@ data class Message(val author :String,val body:String)
 
 @Composable
 fun MessageCard(msg : Message ) {
-    Column {
-        Text(text = msg.author)
-        Text(text = msg.body )
+    Row(modifier = Modifier.padding(all=8.dp)) {
+        Image(painter = painterResource(R.drawable.emily ),
+            contentDescription = "Profile pic for emily",
+        modifier = Modifier
+            .size(40.dp)
+            .clip(CircleShape))
+        Spacer(modifier = Modifier.width(width = 8.dp))
+        Column {
+            Text(text = msg.author)
+            Spacer(modifier = Modifier.height(height = 8.dp))
+
+            Text(text = msg.body )
+        } 
     }
+    
 
 
 }
