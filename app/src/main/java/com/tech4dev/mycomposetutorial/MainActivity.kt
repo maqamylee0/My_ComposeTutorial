a574e16d0f261a15cb3588099eca9de73f0ec027
maqamylee0 <maqamylee0@gmail.com>
2022-11-04 18:12:06 +0300

    Add Column

--- app/src/main/java/com/tech4dev/mycomposetutorial/MainActivity.kt
+++ app/src/main/java/com/tech4dev/mycomposetutorial/MainActivity.kt
@@ -3,6 +3,7 @@ package com.tech4dev.mycomposetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
@@ -11,20 +12,26 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-                    MessageCard("Android")
+                    MessageCard(Message("emily","Welcome to my gallery"))
         }
     }
 }
 
 
+data class Message(val author :String,val body:String)
 
 @Composable
-fun MessageCard(name : String){
-    Text(text = "hello $name")
+fun MessageCard(msg : Message ) {
+    Column {
+        Text(text = msg.author)
+        Text(text = msg.body )
+    }
+
+
 }
 
 @Preview
 @Composable
 fun PreviewMessageCard(){
-    MessageCard(name = "Android")
+    MessageCard(msg = Message("Emily","Welcome to my gallery"))
 }
